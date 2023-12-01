82801b9c7520fbfc12578efa778e0d513c378a2e
57U <62684955+57UU@users.noreply.github.com>
2023-12-01 09:38:41 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 
-const CACHE_VERSION = '3'
+const CACHE_VERSION = '4'
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${CACHE_VERSION}`;
 const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.txt/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
